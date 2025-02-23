d0f470fbd3f53180c7f03b6c264df079712e2819
91andres <91andresmunoz@gmail.com>
2025-02-23 13:12:32 -0500

    📌 [Models] Creación del modelo flights, reservations y migraciones

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    available_seats = models.IntegerField()
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from flights.models import Flight
 
-# Create your models here.
+User = get_user_model()
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
